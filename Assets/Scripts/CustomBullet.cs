8bf50dacf240918dc80af1773501662e6c92573c
AsianPope <113379430+AsianPope@users.noreply.github.com>
2022-10-05 12:17:52 -0500

    Fully added projectile based weapon

---
+++ Assets/Scripts/CustomBullet.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBullet : MonoBehaviour
+{
+    //Assignables
+    public Rigidbody rb;
+    public GameObject explosion;
+    public LayerMask whatIsEnemies;
+
+    //Stats
+    [Range(0f, 1f)]
+    public float bounciness;
+    public bool useGravity;
+
+    //Damage
+    public int explosionDamage;
+    public float explosionRange;
+
+    //Lifetime
+    public int maxCollisions;
+    public float maxLifeTime;
+    public bool explodeOnTouch = true;
+
+    int collisions;
+    PhysicMaterial physics_mat;
+
+    private void start()
+    {
+        Setup();
+    } 
+
+    private void Update()
+    {
+        //when to explode
+        if (collisions > maxCollisions) Explode();
+
+        //Count down lifetime
+        maxLifeTime -= Time.deltaTime;
+        if (maxLifeTime <= 0) Explode();
+    }
+
+    private void Explode()
+    {
+        //Instantiate explosion
+        if (explosion != null) Instantiate(explosion, transform.position, Quaternion.identity);
+
+        //Check for enemies
+        Collider[] enemies = Physics.OverlapSphere(transform.position, explosionRange, whatIsEnemies);
+        for(int i = 0; i<enemies.Length; i++)
+        {
+            //Get component of enemy and call Take Damage
+
+            //example
+            //enemies[i].GetComponent<ShootingAi>().TakeDamage(explosionDamage);
+        }
+
+        //add a little delay, just to make everything works good
+        Invoke("Delay", 0.05f);
+    }
+
+    private void Delay()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Don't count collisions with other bullets
+        if (collision.collider.CompareTag("Bullet")) return;//change
+
+        //Count up collisions
+        collisions++;
+
+        //Explode if bullet hits an enemy directly and explodeOnTouch is activated
+        if (collision.collider.CompareTag("Enemy") && explodeOnTouch) Explode();
+    }
+
+    private void Setup()
+    {
+        //create a new Physic material
+        physics_mat = new PhysicMaterial();
+        physics_mat.bounciness = bounciness;
+        physics_mat.frictionCombine = PhysicMaterialCombine.Minimum;
+        physics_mat.bounceCombine = PhysicMaterialCombine.Maximum;
+        //Assign material to collider
+        GetComponent<SphereCollider>().material = physics_mat;
+
+        //Set gravity
+        rb.useGravity = useGravity;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRange);
+    }
+}
